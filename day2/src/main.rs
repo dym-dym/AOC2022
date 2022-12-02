eb400cdee00c65828a6d3d1f1b99606643638bee
Bettendroffer Dylan <dylan.bettendroffer@etu.umontpellier.fr>
2022-12-02 22:32:04 +0100

    day2 WIP

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+fn part1(input: &str) -> i32 {
+    0
+}
+
+fn part2(input: &str) -> i32 {
+    0
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use crate::{part1, part2};
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("inputtest.txt"), 15);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2("inputtest.txt"), 0);
+    }
 }

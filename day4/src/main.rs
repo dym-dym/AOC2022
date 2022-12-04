b0f1352a1296b5e2c89d51faea5ea190e4de93cd
Dylan Bettendroffer <dymderce@gmail.com>
2022-12-04 15:05:02 +0100

    day4 part1

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,95 @@
+use std::fs;
+
+#[derive(Debug, Copy, Clone)]
+struct Range {
+    beginning: u32,
+    end: u32,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct ElfPair {
+    first_elf: Range,
+    second_elf: Range,
+}
+
+impl Range {
+    pub fn parse(input: &str) -> Range {
+        let mut input = input.split("-");
+
+        let beginning = input.next().unwrap().parse().unwrap();
+        let end = input.last().unwrap().parse().unwrap();
+
+        Range { beginning, end }
+    }
+
+    pub fn fully_contains(&self, pal: &Range) -> bool {
+        let res = self.beginning <= pal.beginning && self.end >= pal.end;
+
+        res
+    }
+}
+
+impl ElfPair {
+    pub fn parse(input: &str) -> ElfPair {
+        // Accounting for split generating a last empty value for some reason
+        if input == "" {
+            return ElfPair {
+                first_elf: Range {
+                    beginning: 0,
+                    end: 0,
+                },
+                second_elf: Range {
+                    beginning: 0,
+                    end: 0,
+                },
+            };
+        }
+        let mut input = input.split(",");
+
+        let first_elf = Range::parse(input.next().unwrap());
+        let second_elf = Range::parse(input.last().unwrap());
+
+        ElfPair {
+            first_elf,
+            second_elf,
+        }
+    }
+}
+
+fn part1(input: &str) -> u32 {
+    let contents = fs::read_to_string(input).unwrap();
+
+    let contents: Vec<&str> = contents.split("\n").collect();
+
+    let contents: Vec<ElfPair> = contents
+        .into_iter()
+        .map(|line| ElfPair::parse(line))
+        .collect();
+
+    let res: u32 = contents
+        .into_iter()
+        .filter(|pair| {
+            pair.first_elf.fully_contains(&pair.second_elf)
+                || pair.second_elf.fully_contains(&pair.first_elf)
+        })
+        .count()
+        .try_into()
+        .unwrap();
+
+    // Returning our result with the last empty value excluded
+    res - 1
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", part1("input.txt"));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::part1;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1("inputtest.txt"), 2)
+    }
 }
